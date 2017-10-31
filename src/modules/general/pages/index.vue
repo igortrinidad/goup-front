4b22cf63ce2fc0f7059ed1effd710e098873f8f2
Andre Brandao <andrebf4@gmail.com>
2017-10-31 18:57:45 -0200

    example of interface elements

--- src/modules/general/pages/index.vue
+++ src/modules/general/pages/index.vue
@@ -1,6 +1,40 @@
 <template>
-    <div class="first-container">
-        hEYYYYYYYYYYYy
+    <div class="main">
+        <div class="container">
+
+            <h1 class="title m-b-30">Buttons</h1>
+
+            <button type="button" class="btn btn-block btn-default">Default</button>
+            <button type="button" class="btn btn-block btn-primary">Primary</button>
+            <button type="button" class="btn btn-block btn-success">Success</button>
+            <button type="button" class="btn btn-block btn-info">Info</button>
+            <button type="button" class="btn btn-block btn-warning">Warning</button>
+            <button type="button" class="btn btn-block btn-danger">Danger</button>
+
+            <hr>
+
+            <h1 class="title m-b-30">Card</h1>
+
+            <div class="card">
+                <div class="card-header ch-alt">
+                    <h2 class="title">Título do card</h2>
+                </div>
+                <div class="card-body card-padding">
+                    <p>
+                        Some content to card
+                    </p>
+                </div>
+            </div>
+
+            <hr>
+
+            <h1 class="title m-b-30">Form</h1>
+
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Qualquer valor">
+            </div>
+
+        </div>
     </div>
 </template>
 
