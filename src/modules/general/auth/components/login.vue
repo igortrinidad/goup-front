c772032da8b00c88be386a4bc7d5f30ab5cabe26
Andre Brandao <andrebf4@gmail.com>
2017-11-01 13:10:49 -0200

    start a test to translations

--- src/modules/general/auth/components/login.vue
+++ src/modules/general/auth/components/login.vue
@@ -1,14 +1,41 @@
 <template lang="html">
     <div class="main">
         <div class="container">
-            <h1 class="title">Login</h1>
+            <h1 class="title">{{ translations.title }}</h1>
         </div>
     </div>
 </template>
 
 <script>
+    import * as translations from '@/translations/login'
+
     export default {
-        name: 'login'
+        name: 'login',
+
+        data() {
+            return{
+                language: 'es',
+                translations: {}
+            }
+        },
+
+        mounted() {
+            this.setCurrentLanguage()
+        },
+
+        methods: {
+            setCurrentLanguage() {
+                if (this.language === 'en') {
+                    this.translations = translations.en
+                }
+                if (this.language === 'pt') {
+                    this.translations = translations.pt
+                }
+                if (this.language === 'es') {
+                    this.translations = translations.es
+                }
+            }
+        }
     }
 </script>
 
---
+++ src/translations/login.js
@@ -0,0 +1,11 @@
+export const en = {
+    title: 'Login'
+}
+
+export const pt = {
+    title: 'Entrar'
+}
+
+export const es = {
+    title: 'Iniciar sesión'
+}
