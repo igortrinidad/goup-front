d355b1f3258544dc6746e21701e03f034b22e0d6
Andre Brandao <andrebf4@gmail.com>
2017-11-07 19:11:50 -0200

    start profile/edit

--- src/models/User.js
+++ src/models/User.js
@@ -2,9 +2,19 @@ export default {
     id: '123123',
     name: 'John',
     last_name: 'Doe',
+    full_name: 'John Doe',
     slug: 'john-doe',
     age: '23',
     avatar: 'https://maisbartenders.com.br/img/portfolio/My_garden_Mais_Bartenders.png',
-
-    full_name: 'John Doe'
+    photos: [
+        {
+            photo_url: 'https://maisbartenders.com.br/img/portfolio/My_garden_Mais_Bartenders.png'
+        },
+        {
+            photo_url: 'https://maisbartenders.com.br/img/portfolio/My_garden_Mais_Bartenders.png'
+        },
+        {
+            photo_url: 'https://maisbartenders.com.br/img/portfolio/My_garden_Mais_Bartenders.png'
+        }
+    ],
 }
--- src/modules/general/user/components/dashboard/edit-profile.vue
+++ src/modules/general/user/components/dashboard/edit-profile.vue
@@ -9,7 +9,27 @@
 
         <transition appear mode="in-out" enter-active-class="animated fadeInRight" leave-active-class="animated fadeOutRight">
             <div class="main">
-                <h1 class="text-center">Editar o perfil do usuario</h1>
+
+            <!-- Photos -->
+            <div class="p-relative">
+                <div class="swiper-container swiper-user-photos" ref="userPhotos">
+                    <div class="swiper-wrapper">
+                        <div
+                            class="swiper-slide"
+                            v-for="(photo, index) in user.photos"
+                            :style="{ backgroundImage: `url(${ photo.photo_url })` }"
+                            :key="index"
+                        >
+                        </div>
+                    </div>
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
+            <!-- / Photos -->
+
             </div>
         </transition>
 
@@ -57,7 +77,49 @@
 
             getUser() {
                 this.user = User
+                console.log(this.user);
+                this.initSwiper()
             },
+
+            initSwiper() {
+                let that = this
+
+                setTimeout(() => {
+                    that.swiperUserPhotos = new Swiper(that.$refs.userPhotos, {
+                        spaceBetween: 15,
+                        slidesPerView: 1,
+                        nextButton: '.swiper-button-next',
+                        prevButton: '.swiper-button-prev',
+                        pagination: '.swiper-pagination',
+                        paginationClickable: true,
+                    })
+                }, 200);
+            }
         }
     }
 </script>
+
+<style scoped>
+    .swiper-container.swiper-user-photos { margin-top: -15px; }
+    .swiper-container.swiper-user-photos .swiper-slide {
+        width: 100%;
+        height: 350px !important;
+        display: block;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center center;
+    }
+
+    .swiper-container.swiper-user-photos .swiper-button-next,
+    .swiper-container.swiper-user-photos .swiper-button-prev {
+        background-image: none;
+        width: 40%;
+        top: 0;
+        height: 100%;
+        margin-top: 0;
+    }
+    .swiper-container.swiper-user-photos .swiper-button-next { right: 0; }
+    .swiper-container.swiper-user-photos .swiper-button-prev { left: 0; }
+
+    .swiper-pagination { width: 100%; }
+</style>
