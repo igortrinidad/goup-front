2e64fc1d7b3cdf4def7d9f1ffb75f9cb3f2aee69
Andre Brandao <andrebf4@gmail.com>
2017-11-16 15:20:29 -0200

    start modal

--- src/modules/general/places/components/list.vue
+++ src/modules/general/places/components/list.vue
@@ -12,7 +12,14 @@
                 <div class="container">
 
                     <div class="text-center">
-                        <button type="button" class="btn btn-primary transparent">Filtro</button>
+                        <button
+                            type="button"
+                            class="btn btn-primary transparent"
+                            data-toggle="modal"
+                            data-target="#modal-filter"
+                        >
+                            Filtro
+                        </button>
                     </div>
 
                     <div class="row m-t-30">
@@ -43,6 +50,28 @@
                     </div>
 
                 </div>
+
+                <!-- Modal Filter -->
+                <div class="modal" id="modal-filter" tabindex="-1" role="dialog" aria-labelledby="modal-filter" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                ...
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- / Modal Filter -->
             </div>
         </transition>
 
