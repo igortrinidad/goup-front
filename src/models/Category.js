7add3161d7bfedf8c1c0166fe79b7b9c02445e5e
Igor Trindade <igorlucast@hotmail.com>
2017-11-16 16:15:29 -0200

    Added category of places model array

---
+++ src/models/Category.js
@@ -0,0 +1,22 @@
+
+export function cleanCategoriesArrayExample() {
+	return [
+		{
+			name_en: 'Bar',
+			name_pt: 'Bar',
+		},
+		{
+			name_en: 'Restaurant',
+			name_pt: 'Restaurant',
+		},
+		{
+			name_en: 'Concert',
+			name_pt: 'Show',
+		},
+		{
+			name_en: 'Club',
+			name_pt: 'Balada',
+		},
+
+	]
+}
