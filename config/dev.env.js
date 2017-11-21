702b6325ca50ae583f54f03741f50d256dbdb9e9
Igor Trindade <igorlucast@hotmail.com>
2017-11-21 21:15:44 -0200

    Change app url config

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,8 +3,8 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    API_URL: '"http://goup.dev/api"',
-    API_ROOT_URL: '"http://goup.dev/"',
+    API_URL: '"http://goup-back.dev/api"',
+    API_ROOT_URL: '"http://goup-back.dev/"',
     FACEBOOK_CLIENT_ID: '"171329556786695"',
     GOOGLE_MAPS_KEY: '"AIzaSyAc7FRXAfTUbAG_lUOjKzzFa41JbRCCbbM"'
 })
